9502cdd32602db837e3ce53c18edfc05c6eecd74
cinesubzindex1 <cinesubzindex1@gmail.com>
2024-08-02 19:46:43 +0530

    Update megaAccounts.js

--- utils/megaAccounts.js
+++ utils/megaAccounts.js
@@ -1,4 +1,4 @@
 module.exports = [
     {id:'main',email:'csheroku01@gmail.com',password:'Heroku@1995'},
- // {id:'eg',email:'eg@gmail.com',password:'eg'},
+ // {id:'eg',email:'eg@gmail.com',password:'eg'}, 
 ]
